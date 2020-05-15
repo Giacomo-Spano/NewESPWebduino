9a45fa7193d5fe2a199ebdc2de224841f8a22fef
GIACOMO SPANO <giacomo.spano@libero.it>
2020-05-15 22:50:25 +0200

    Add project files.

---
+++ NewESPWebduino/NewESPWebduino.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		NewESPWebduino.ino
+ Created:	5/15/2020 10:50:11 PM
+ Author:	giaco
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
